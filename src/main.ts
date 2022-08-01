2535a4715c6d092dfd915692b333246a76d58333
KowalskiTom <tom.kowalski@daysmart.com>
2022-08-01 11:58:52 -0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,81 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+    const env = core.getInput('env')
+    const context = github.context
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const request = await octokit.rest.repos.listDeployments({
+      ...context.repo,
+      env
+    })
 
-    core.setOutput('time', new Date().toTimeString())
+    const deployments = request.data
+
+    if (deployments.length > 0) {
+      core.setOutput('deployment_url', deployments[0].url.toString())
+      core.setOutput('deployment_id', deployments[0].id.toString())
+      core.setOutput('deployment_node_id', deployments[0].node_id.toString())
+      core.setOutput('deployment_sha', deployments[0].sha.toString())
+      core.setOutput('deployment_ref', deployments[0].ref.toString())
+      core.setOutput('deployment_task', deployments[0].task.toString())
+      core.setOutput('deployment_payload', deployments[0].payload.toString())
+      core.setOutput(
+        'deployment_environment',
+        deployments[0].environment.toString()
+      )
+      if (deployments[0].description) {
+        core.setOutput(
+          'deployment_description',
+          deployments[0].description.toString()
+        )
+      }
+      if (deployments[0].creator) {
+        core.setOutput(
+          'deployment_creator_login',
+          deployments[0].creator.login.toString()
+        )
+      }
+      if (deployments[0].creator) {
+        core.setOutput(
+          'deployment_creator_id',
+          deployments[0].creator.id.toString()
+        )
+      }
+      core.setOutput(
+        'deployment_created_at',
+        deployments[0].created_at.toString()
+      )
+      core.setOutput(
+        'deployment_updated_at',
+        deployments[0].updated_at.toString()
+      )
+      core.setOutput(
+        'deployment_statuses_url',
+        deployments[0].statuses_url.toString()
+      )
+      core.setOutput(
+        'deployment_repository_url',
+        deployments[0].repository_url.toString()
+      )
+      if (deployments[0].transient_environment) {
+        core.setOutput(
+          'deployment_transient_environment',
+          deployments[0].transient_environment.toString()
+        )
+      }
+      if (deployments[0].production_environment) {
+        core.setOutput(
+          'deployment_production_environment',
+          deployments[0].production_environment.toString()
+        )
+      }
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
-
 run()
